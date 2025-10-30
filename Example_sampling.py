6939da972dda9403b07bebb8ddf38dab38501eb9
sarahgrosshagauer <sarah.grosshagauer@gmail.com>
2025-10-30 15:43:57 +0100

    V1.0 upload

--- Example_sampling.py
+++ Example_sampling.py
@@ -17,16 +17,17 @@ import APSim
 apsim = APSim.APSim()
 apsim.subpath = 'XXXXXXXXX'
 apsim.fnamecoil = 'XXX/simnibs/4.0.0/resources/coil_models/Drakaki_BrainStim_2022/MagMore_PMD70.ccd'
-apsim.target = mni2subject_coords([-38,44,26], tms_opt.subpath)
+apsim.target = mni2subject_coords([-38,44,26], apsim.subpath)
 
 apsim.pathfem = 'APSim_test'
 apsim.below_skin_distance = 2
-apsim.search_radius = 10
 apsim.above_skin_distance = 10
+apsim.search_radius = 10
 apsim.angle_sampling = 1
 apsim.spatial_resolution = 2
 apsim.method = 'ADM'
 apsim.run()
+
 
 
 
