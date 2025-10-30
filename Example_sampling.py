f0f50fc002a590dc66c8331f3ddc6117929347ed
sarahgrosshagauer <sarah.grosshagauer@gmail.com>
2025-10-30 15:46:43 +0100

    Update Example_sampling.py

--- Example_sampling.py
+++ Example_sampling.py
@@ -23,10 +23,11 @@ apsim.pathfem = 'APSim_test'
 apsim.below_skin_distance = 2
 apsim.above_skin_distance = 10
 apsim.search_radius = 10
-apsim.angle_sampling = 1
-apsim.spatial_resolution = 1 #needs to be 0 (=72 quaternions) or 1 (=576 quaternions)
+apsim.angle_sampling = 1 #needs to be 0 (=72 quaternions) or 1 (=576 quaternions)
+apsim.spatial_resolution = 2 
 apsim.method = 'ADM'
 apsim.run()
+
 
 
 
